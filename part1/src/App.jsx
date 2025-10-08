d1e1991567793baa68756f1f0850d7b20543bda0
Keiji <BB123021@sun.ac.jp>
2025-10-08 10:21:51 +0900

    part1-1 complete

---
+++ part1/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greetings app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = ['Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+export default App
